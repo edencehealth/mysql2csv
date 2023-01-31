7c64d313507cb5943021d6cfe0a71fcd9ea43eed
actualben <actualben@users.noreply.github.com>
2023-01-31 06:52:04 +0100

    implement configurable chunksize; improve documentation

--- README.md
+++ README.md
@@ -15,39 +15,50 @@ The program produces the following usage documentation when invoked with the
 `--help` and `-h` command-line arguments:
 
 ```
-usage: mysql2csv [-h] [--user USER] [--password PASSWORD] [--host HOST]
-                 [--database DATABASE] [--path PATH]
-                 [--csvdialect {excel,excel-tab,unix}]
-                 [--csvencoding CSVENCODING]
-                 [--loglevel {CRITICAL,FATAL,ERROR,WARN,WARNING,INFO,DEBUG}]
+usage: mysql2csv [-h] [--loglevel LOG_LEVEL] [--host DB_HOST] [--user DB_USER]
+                 [--password DB_PASSWORD] [--database DB_DATABASE]
+                 [--chunksize CHUNK_SIZE] [--path OUTPUT_PATH]
+                 [--csvdialect CSV_DIALECT] [--csvencoding CSV_ENCODING]
                  table_name [table_name ...]
 
 generates CSV files from MySQL/MariaDB database tables
 
 positional arguments:
-  table_name            the name of the tables to create csv files for
+  table_name            name of database table to copy into local CSV output
+                        file (multiple can be specified)
 
 options:
   -h, --help            show this help message and exit
-  --user USER           username to use when connecting to the database
-                        (default: root)
-  --password PASSWORD   password string to use when connecting to the database
-                        (default: password)
-  --host HOST           network host to use when connecting to the database
+  --loglevel LOG_LEVEL  determines how verbosely to log program operation;
+                        possible values: "CRITICAL", "FATAL", "ERROR", "WARN",
+                        "WARNING", "INFO", "DEBUG" (default: INFO)
+  --host DB_HOST        network host to use when connecting to the database
                         (default: 127.0.0.1)
-  --database DATABASE   database name to use after connecting to the database
+  --user DB_USER        username to use when connecting to the database
                         (default: root)
-  --path PATH           the base output path to use when creating csv files
+  --password DB_PASSWORD
+                        password string to use when connecting to the database
+                        (default: password)
+  --database DB_DATABASE
+                        database name to use after connecting to the database
+                        (default: root)
+  --chunksize CHUNK_SIZE
+                        data retrieved from the server will be returned in
+                        chunks of this many rows; using a larger chunk size
+                        generally results in faster operation but uses more
+                        memory; selecting an excessive size will result in the
+                        program running out of memory (default: 1000)
+  --path OUTPUT_PATH    the base output path to use when creating CSV files
                         (default: /output)
-  --csvdialect {excel,excel-tab,unix}
-                        the python csv.writer dialect; see:
+  --csvdialect CSV_DIALECT
+                        python csv.writer dialect; possible values: "excel",
+                        "excel-tab", "unix"; see:
                         https://docs.python.org/3/library/csv.html#csv.Dialect
                         (default: unix)
-  --csvencoding CSVENCODING
-                        the character encoding to use when writing the CSV
-                        files (default: utf8)
-  --loglevel {CRITICAL,FATAL,ERROR,WARN,WARNING,INFO,DEBUG}
-                        the python logging level to use (default: INFO)
+  --csvencoding CSV_ENCODING
+                        character encoding used when writing CSV files
+                        (default: utf8)
+
 ```
 
 ### Docker Compose Configuration
@@ -58,6 +69,7 @@ Here is an example of how the image can be deployed in a docker compose file:
 services:
   mysql2csv:
     environment:
+      CHUNK_SIZE: 100000
       CSV_DIALECT: "excel"  # see also: unix ; https://docs.python.org/3/library/csv.html
       DB_DATABASE: "mysql"
       DB_HOST: "db"
--- src/mysql2csv/__main__.py
+++ src/mysql2csv/__main__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-""" utility for exporting mysql tables to csv files  """
+""" utility for exporting mysql tables to CSV files  """
 import argparse
 import csv
 import logging
 import os
 import sys
-from typing import Sequence
+from typing import Final, Sequence
 
 import mariadb
+
+LOG_FORMAT: Final = "%(asctime)s %(levelname)s %(message)s"
 
 
 def dump_tables(
@@ -16,107 +18,169 @@ def dump_tables(
     output_dir: str,
     dialect: str,
     encoding: str = "utf8",
+    chunksize: int = 1000,
 ):
     """
     given a database server connection and a list of table names, write a CSV
     file containing the complete contents of each table into the given output
     directory; the given csv.Dialect and character encoding are used to write
-    the csv output files
+    the CSV output files
     """
     for table_name in tables:
         logging.debug("dumping table: %s", table_name)
         cur = cnxn.cursor()
+        cur.arraysize = chunksize
 
         # attempting to prevent injection... this could be improved
         esc_table_name = cnxn.escape_string(table_name)
 
-        cur.execute(f"SELECT * FROM {esc_table_name}")  # nosec: escaping value
+        cur.execute(
+            f"SELECT * FROM {esc_table_name}",  # nosec: escaping value
+            buffered=False,
+        )
         fieldnames = [i[0] for i in cur.description]
-        data = cur.fetchall()
 
         output_filename = os.path.join(output_dir, table_name + ".csv")
         with open(output_filename, "w", newline="", encoding=encoding) as csvfile:
             writer = csv.writer(csvfile, dialect=dialect)
             writer.writerow(fieldnames)
-            writer.writerows(data)
-        logging.info("wrote to file: %s", output_filename)
+            while data := cur.fetchmany():
+                writer.writerows(data)
+                logging.info(
+                    "%s: wrote %s-row chunk to %s",
+                    table_name,
+                    len(data),
+                    output_filename,
+                )
+        logging.info(
+            "%s: finished dumping table to %s (%s rows)",
+            table_name,
+            output_filename,
+            cur.rowcount,
+        )
+
+
+def quoted_list(input: Sequence[str]) -> str:
+    """
+    return a joined, comma-separated version of the given list of strings with
+    each member in double quotes; NOTE: NOT SAFE FOR UNTRUSTED INPUTS!
+    """
+    return ", ".join([f'"{item}"' for item in input])
 
 
 def main() -> int:
     """
     entrypoint for direct execution; returns an integer suitable for use with sys.exit
     """
+    log_level_choices = list(logging._nameToLevel)[:-1]
+    dialect_choices = csv.list_dialects()
+
     argp = argparse.ArgumentParser(
         prog=__package__,
         description=("generates CSV files from MySQL/MariaDB database tables"),
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     argp.add_argument(
+        "--loglevel",
+        default=os.environ.get("LOG_LEVEL", "INFO"),
+        metavar="LOG_LEVEL",
+        choices=log_level_choices,
+        help=(
+            "determines how verbosely to log program operation; "
+            f"possible values: {quoted_list(log_level_choices)}"
+        ),
+    )
+    argp.add_argument(
+        "--host",
+        default=os.environ.get("DB_HOST", "127.0.0.1"),
+        metavar="DB_HOST",
+        help="network host to use when connecting to the database",
+    )
+    argp.add_argument(
         "--user",
         default=os.environ.get("DB_USER", "root"),
+        metavar="DB_USER",
         help="username to use when connecting to the database",
     )
     argp.add_argument(
         "--password",
         default=os.environ.get("DB_PASSWORD", "password"),
+        metavar="DB_PASSWORD",
         help="password string to use when connecting to the database",
-    )
-    argp.add_argument(
-        "--host",
-        default=os.environ.get("DB_HOST", "127.0.0.1"),
-        help="network host to use when connecting to the database",
     )
     argp.add_argument(
         "--database",
         default=os.environ.get("DB_DATABASE", "root"),
+        metavar="DB_DATABASE",
         help="database name to use after connecting to the database",
+    )
+    argp.add_argument(
+        "--chunksize",
+        type=int,
+        default=int(os.environ.get("CHUNK_SIZE", "1000")),
+        metavar="CHUNK_SIZE",
+        help=(
+            "data retrieved from the server will be returned in chunks of this many "
+            "rows; using a larger chunk size generally results in faster operation "
+            "but uses more memory; selecting an excessive size will result in the "
+            "program running out of memory"
+        ),
     )
     argp.add_argument(
         "--path",
         default=os.environ.get("OUTPUT_PATH", os.getcwd()),
-        help="the base output path to use when creating csv files",
+        metavar="OUTPUT_PATH",
+        help="the base output path to use when creating CSV files",
     )
     argp.add_argument(
         "--csvdialect",
         default=os.environ.get("CSV_DIALECT", "unix"),
-        choices=csv.list_dialects(),
+        metavar="CSV_DIALECT",
+        choices=dialect_choices,
         help=(
-            "the python csv.writer dialect; "
+            "python csv.writer dialect; "
+            f"possible values: {quoted_list(dialect_choices)}; "
             "see: https://docs.python.org/3/library/csv.html#csv.Dialect"
         ),
     )
     argp.add_argument(
         "--csvencoding",
         default=os.environ.get("CSV_ENCODING", "utf8"),
-        help="the character encoding to use when writing the CSV files",
-    )
-    argp.add_argument(
-        "--loglevel",
-        default=os.environ.get("LOG_LEVEL", "INFO"),
-        choices=list(logging._nameToLevel)[:-1],
-        help="the python logging level to use",
+        metavar="CSV_ENCODING",
+        help="character encoding used when writing CSV files",
     )
     argp.add_argument(
         "table_name",
         nargs="+",
-        help="the name of the tables to create csv files for",
+        help=(
+            "name of database table to copy into local CSV output file (multiple "
+            "can be specified)"
+        ),
     )
     args = argp.parse_args()
-    logging.basicConfig(level=args.loglevel)
+    logging.basicConfig(
+        level=args.loglevel,
+        format=LOG_FORMAT,
+    )
 
-    with mariadb.connect(
-        user=args.user,
-        password=args.password,
-        host=args.host,
-        database=args.database,
-    ) as cnxn:
-        dump_tables(
-            cnxn,
-            args.table_name,
-            args.path,
-            args.csvdialect,
-            args.csvencoding,
-        )
+    try:
+        with mariadb.connect(
+            user=args.user,
+            password=args.password,
+            host=args.host,
+            database=args.database,
+        ) as cnxn:
+            dump_tables(
+                cnxn,
+                args.table_name,
+                args.path,
+                args.csvdialect,
+                encoding=args.csvencoding,
+                chunksize=args.chunksize,
+            )
+    except mariadb.Error as e:
+        logging.error("database exception: %s", e)
+        return 1
 
     return 0
 
